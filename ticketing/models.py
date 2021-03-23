593411602a996485219e925ebc6b8870e2860de7
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-03-23 20:42:47 +0430

    Add verbose_name

--- ticketing/models.py
+++ ticketing/models.py
@@ -1,35 +1,50 @@
 from django.db import models
 
 class Movie(models.Model):
-    name = models.CharField(max_length = 100)
-    director =  models.CharField(max_length = 50)
-    year = models.IntegerField()
-    length = models.IntegerField()
-    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'فیلم'
+        verbose_name_plural = 'فیلم'
+
+    name = models.CharField('عنوان', max_length = 100)
+    director =  models.CharField('کارگردان', max_length = 50)
+    year = models.IntegerField('سال ساخت')
+    length = models.IntegerField('زمان')
+    description = models.TextField('توضیحات')
 
     def __str__(self):
         return self.name
 
 
 class Cinema(models.Model):
-    cinema_code = models.IntegerField(primary_key = True)
-    name = models.CharField(max_length = 50)
-    city = models.CharField(max_length = 30, default = 'تهران')
-    capacity = models.IntegerField()
-    phone = models.CharField(max_length = 20, null=True)
-    address = models.TextField()
+
+    class Meta:
+        verbose_name = 'سینما'
+        verbose_name_plural = 'سینما'
+
+    cinema_code = models.IntegerField('کد', primary_key = True)
+    name = models.CharField('نام', max_length = 50)
+    city = models.CharField('شهر', max_length = 30, default = 'تهران')
+    capacity = models.IntegerField('ظرفیت')
+    phone = models.CharField('تلفن', max_length = 20, null=True)
+    address = models.TextField('آدرس')
 
     def __str__(self):
         return self.name
 
 
 class ShowTime(models.Model):
-    movie = models.ForeignKey('Movie', on_delete = models.PROTECT)
-    cinema = models.ForeignKey('Cinema', on_delete=models.PROTECT)
-    start_time = models.DateTimeField()
-    price = models.IntegerField()
-    salable_seats = models.IntegerField()
-    free_seats = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'سانس'
+        verbose_name_plural = 'سانس'
+
+    movie = models.ForeignKey('Movie', on_delete = models.PROTECT, verbose_name = 'فیلم')
+    cinema = models.ForeignKey('Cinema', on_delete=models.PROTECT, verbose_name = 'سینما')
+    start_time = models.DateTimeField('زمان شروع')
+    price = models.IntegerField('قیمت')
+    salable_seats = models.IntegerField('صندلی های قابل فروش')
+    free_seats = models.IntegerField('صندلی های خالی')
 
     SALE_NOT_STARTED = 1
     SALE_OPEN = 2
@@ -46,7 +61,7 @@ class ShowTime(models.Model):
         (SHOW_CANCELED, 'سانس لغو شد'),
     )
 
-    status = models.IntegerField(choices=status_choices)
+    status = models.IntegerField('وضعیت', choices=status_choices)
 
     def __str__(self):
         return '{} - {} - {}'.format(self.movie, self.cinema, self.start_time)
