d35a6b5ccfa721bd6ff19d98d904ae049041fc37
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-03-24 13:12:38 +0430

    Simple Show fields with ul and li html tags in views.py

--- ticketing/views.py
+++ ticketing/views.py
@@ -5,7 +5,23 @@ from .models import Movie
 
 def movie_list(request):
     movies = Movie.objects.all()
-    response_text = '\n'.join(
-        '{}: {}'.format(i, movie) for i,movie in enumerate(movies, start=1)
-    )
+    # response_text = '\n'.join(
+    #     '{}: {}'.format(i, movie) for i,movie in enumerate(movies, start=1)
+    # )
+
+    response_text = """
+        <html>
+        <head>
+        <title> لیست سینماها </title>
+        </head>
+        <body>
+            <h1>فهرست سینماهای کشور</h1>
+            <ul>
+                {}
+            </ul>
+        </body>
+        </html>
+        """.format(
+            '\n'.join('<li>{}</li>'.format(movie) for movie in movies)
+        )
     return HttpResponse(response_text)
