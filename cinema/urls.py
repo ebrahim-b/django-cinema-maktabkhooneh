9dabdcbc9b119a6758c4640afca8fed07dfa7d5d
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-03-24 13:04:35 +0430

    Simple Show movies in browser

--- cinema/urls.py
+++ cinema/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ticketing import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('movie/list/', views.movie_list)
 ]
--- ticketing/views.py
+++ ticketing/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
 
-# Create your views here.
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    response_text = '\n'.join(
+        '{}: {}'.format(i, movie) for i,movie in enumerate(movies, start=1)
+    )
+    return HttpResponse(response_text)
