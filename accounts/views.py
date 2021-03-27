8c88755808c862a5ec5c5b49f64f89065a9799e1
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-03-27 17:34:37 +0430

    Add login & logout view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 def login_view(request):
-    return render(request,'accounts/login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username = username, password = password)
+        if user is None:
+            #return raise Http404("نام کاربری یا گذرواژه نادرست می باشد")
+            context = {
+                'username': username,
+                'error' : 'کاربری با این مشخصات یافت نشد',
+            }
+        else:
+            login(request, user)
+            #return HttpResponse('خوش آمدید')
+            return HttpResponseRedirect(reverse('ticketing:showtime_list'))
+    else:
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('ticketing:showtime_list'))
+        context= {}
+    return render(request,'accounts/login.html', context)
 
 def logout_view(request):
-    pass
+    logout(request)
+    return HttpResponseRedirect(reverse('accounts:login'))
